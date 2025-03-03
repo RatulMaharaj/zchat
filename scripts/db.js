4b33eb20dc635ce42a3fc9f01d655fd81de4a62a
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2025-03-03 12:30:38 +0200

    refactor: :recycle: Update db create script

--- scripts/db.js
+++ scripts/db.js
@@ -2,16 +2,18 @@ import { execSync } from 'child_process';
 import { configDotenv } from 'dotenv';
 
 // Load environment variables from .env file.
-configDotenv();
+configDotenv({
+	path: '.env.production'
+});
 
-// Ensure the ZERO_UPSTREAM_DB environment variable is set.
+// Ensure the DATABASE_URL environment variable is set.
 if (!process.env.DATABASE_URL) {
 	console.error('Error: DATABASE_URL environment variable is not set.');
 	process.exit(1);
 }
 
 // Connect to the server using DATABASE_URL and create a new database.
-const dbs = ['zchat', 'zchat_cvr', 'zchat_cdb'];
+const dbs = ['zchat'];
 dbs.map((db) => {
 	try {
 		execSync(`psql "${process.env.DATABASE_URL}" -c "CREATE DATABASE ${db};"`, {
